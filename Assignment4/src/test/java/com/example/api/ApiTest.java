957b04c2a0b49caf65d9df2d6dfa6749b05d362f
kevanjit singh <singhkevanjit@gmail.com>
2024-12-03 00:20:13 -0500

    Assignment 4 Commit

---
+++ Assignment4/src/test/java/com/example/api/ApiTest.java
@@ -0,0 +1,32 @@
+package com.example.api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ApiTest {
+
+    @Test
+    public void testGetRequest() {
+        Response response = RestAssured.get("https://jsonplaceholder.typicode.com/posts/1");
+        Assert.assertEquals(response.getStatusCode(), 200);
+        // Temporary solution: Just verifying the status code for now
+    }
+    @Test
+    public void testAllPosts() {
+        Response response = RestAssured.get("https://jsonplaceholder.typicode.com/posts");
+        Assert.assertEquals(response.getStatusCode(), 200);
+        Assert.assertTrue(response.getBody().jsonPath().getList("$").size() > 0);
+    }
+    @Test
+    public void testPostRequest() {
+        // Known issue: This endpoint occasionally fails under load
+        Response response = RestAssured.given()
+                .contentType("application/json")
+                .body("{ \"title\": \"foo\", \"body\": \"bar\", \"userId\": 1 }")
+                .post("https://jsonplaceholder.typicode.com/posts");
+        Assert.assertEquals(response.getStatusCode(), 201);
+    }
+
+}
